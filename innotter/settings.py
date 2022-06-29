02f01f7600eac2daa746d9e93b7c604ba995032c
sheirand <eugene.osakovich@gmail.com>
2022-06-29 11:43:08 +0300

    Added dockerized PostgreSQL database

--- innotter/settings.py
+++ innotter/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'innotter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "postgres",
+        'USER': "postgres",
+        'PASSWORD': "postgres",
+        'HOST': '0.0.0.0',
+        'PORT': 5432,
     }
 }
 
