ea35d3d551576aaa5ea0fd577ff3cb52b6c1cec8
sheirand <eugene.osakovich@gmail.com>
2022-06-29 14:55:37 +0300

    Custom User Model: models.py: - custom user model added, custom user manager added settings.py - AUTH USER MODEL - custom user model admin.py - admin panel for custom user model

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.forms import ModelForm
 
-# Register your models here.
+from user import models
+
+
+class UserCreationForm(ModelForm):
+    class Meta:
+        model = models.User
+        fields = ('email',)
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    list_display = ("email",)
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'role', 'is_blocked', 'blocked_to', 'image_path')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'role', 'is_blocked', 'blocked_to', 'image_path')}
+         ),
+    )
+
+    filter_horizontal = ()
+
+
+admin.site.register(models.User, CustomUserAdmin)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CustomUserManager(UserManager):
+    def create_user(self, email: str, password: str, role: str = "user") -> "User":
+        if not email:
+            raise ValueError("email is required")
+        if not password:
+            raise ValueError("password is required")
+        if role not in ["user", "moderator"]:
+            raise ValueError("role must be either \"moderator\" or \"user\"")
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.is_active = True
+        user.role = role
+        if user.role == "moderator":
+            user.is_staff = True
+            user.is_superuser = False
+            user.save()
+            return user
+        user.is_staff = False
+        user.is_superuser = False
+        user.save()
+        return user
+
+    def create_superuser(self, email: str, password: str) -> "User":
+        if not email:
+            raise ValueError("email is required")
+        if not password:
+            raise ValueError("password is required")
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.is_active = True
+        user.role = "admin"
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        USER = "user"
+        MODERATOR = "moderator"
+        ADMIN = "admin"
+    first_name = None
+    last_name = None
+    username = None
+    email = models.EmailField(_("email"), max_length=150, unique=True)
+    image_path = models.CharField(max_length=200, null=True, blank=True)
+    role = models.CharField(max_length=9, choices=Roles.choices)
+    is_blocked = models.BooleanField(
+        _("blocked"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site.")
+    )
+    blocked_to = models.DateTimeField(_("blocked until:"), null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
