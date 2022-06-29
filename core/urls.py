bb3f32c77a0c860dbe619bbe4fd853947ae00aad
sheirand <eugene.osakovich@gmail.com>
2022-06-29 15:16:58 +0300

    added RAW (very RAW version) user endpoint /api/v1/user/

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from user.views import UserAPIView
 
+router = routers.DefaultRouter()
+router.register('user', UserAPIView)
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
 ]
+
