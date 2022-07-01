204a2ee8c4303f775965e5a3f0a2f40b25894bf5
sheirand <eugene.osakovich@gmail.com>
2022-07-01 15:18:59 +0300

    drf-yasg added

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf-yasg'
     'user.apps.UserConfig',
     'page.apps.PageConfig'
 ]
