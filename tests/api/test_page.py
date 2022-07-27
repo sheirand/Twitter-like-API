631f5e2184e39774614b5155303e688e665ec4ad
sheirand <eugene.osakovich@gmail.com>
2022-07-27 14:56:05 +0300

    [TESTS PAGE] add: - pytest ini: filterwarnings - test page added: test for user can update image field; test for user cant update fields on pages owned by another users test for user can delete page he created

--- tests/api/test_page.py
+++ tests/api/test_page.py
@@ -1,5 +1,7 @@
 import pytest
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
+from page.models import Page, Post
 
 
 @pytest.mark.django_db
@@ -71,4 +73,47 @@ def test_user_retrieve_pages(client_with_pages, page_ids, user_token,
     assert data['description'] == page_blocked['description']
     assert data['is_private'] == page_blocked['is_private']
     assert data['is_blocked'] == page_blocked['is_blocked']
+
+
+@pytest.mark.django_db
+def test_user_can_change_page(client_with_pages, page_ids, user_token):
+    payload = dict(
+        image="image-path/image.jpg"
+    )
+
+    response = client_with_pages.patch(f'/api/v1/pages/{page_ids["public_page_id"]}/',
+                                       data=payload, HTTP_AUTHORIZATION=f"{user_token}",
+                                       format='json')
+
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.data
+
+    assert data['image'] == payload["image"]
+
+
+@pytest.mark.django_db
+def test_user_cant_change_another_user_page(client_with_pages, page_ids, user_token):
+
+    response = client_with_pages.patch(f'/api/v1/pages/{page_ids["private_page_id"]}/',
+                                       data={"image": "image.jpeg"},
+                                       HTTP_AUTHORIZATION=f"{user_token}",
+                                       format='json')
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_user_can_delete_his_page(client_with_pages, page_ids, user_token):
+
+    page = Page.objects.get(id=page_ids["public_page_id"])
+
+    response = client_with_pages.delete(f'/api/v1/pages/{page_ids["public_page_id"]}/',
+                                        HTTP_AUTHORIZATION=f"{user_token}")
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    with pytest.raises(ObjectDoesNotExist):
+        page.refresh_from_db()
+
 
